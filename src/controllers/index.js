b70386686553ee4811b3a3a5cfb2bbcf675ab45f
Sachins0 <sachins.ssingh0@gmail.com>
2024-09-25 16:12:52 +0530

    first commit with sequelize orm and mysql2

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+
+
+module.exports={
+    infoController: require('./info-controller')
+}
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express= require('express')
+const { infoController } = require('../../controllers')
+
+const router=express.Router()
+
+router.get('/info',infoController.info)
+
+module.exports=router
